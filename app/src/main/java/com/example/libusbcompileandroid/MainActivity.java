abad381568335a239217716631b4a348f41a6c70
Ibai Gorordo <ibai.gorordo@hotmail.com>
2021-08-29 22:52:14 +0900

    Added compilation script

--- app/src/main/java/com/example/libusbcompileandroid/MainActivity.java
+++ app/src/main/java/com/example/libusbcompileandroid/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.libusbcompileandroid;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.widget.TextView;
 
@@ -9,13 +10,10 @@ import com.example.libusbcompileandroid.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
-    // Used to load the 'libusbcompileandroid' library on application startup.
-    static {
-        System.loadLibrary("libusbcompileandroid");
-    }
 
     private ActivityMainBinding binding;
 
+    @SuppressLint("SetTextI18n")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,12 +23,7 @@ public class MainActivity extends AppCompatActivity {
 
         // Example of a call to a native method
         TextView tv = binding.sampleText;
-        tv.setText(stringFromJNI());
+        tv.setText("Compiled");
     }
 
-    /**
-     * A native method that is implemented by the 'libusbcompileandroid' native library,
-     * which is packaged with this application.
-     */
-    public native String stringFromJNI();
 }
